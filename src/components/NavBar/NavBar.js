5bda6f4d8ebf864c608408b7fe55e50276ab1883
Kateryna Boikova <boykova.ekaterina.i@gmail.com>
2025-02-13 15:02:16 +0000

    add active link highlight

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,17 +2,36 @@ import { NavLink } from 'react-router-dom';
 import styles from './NavBar.module.scss';
 
 function NavBar () {
+  const activeStyle = {
+    textDecoration: 'underline',
+  };
+
   return (
     <nav className={styles.navBar}>
       <ul className={styles.navItemsContainer}>
         <li className={styles.navItem}>
-          <NavLink to='/'>Search</NavLink>
+          <NavLink
+            to='/'
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Search
+          </NavLink>
         </li>
         <li className={styles.navItem}>
-          <NavLink to='/saved'>Saved</NavLink>
+          <NavLink
+            to='/saved'
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Saved
+          </NavLink>
         </li>
         <li className={styles.navItem}>
-          <NavLink to='/folders'>Folders</NavLink>
+          <NavLink
+            to='/folders'
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Folders
+          </NavLink>
         </li>
       </ul>
     </nav>
