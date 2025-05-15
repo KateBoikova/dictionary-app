442a54c4827168344537cdb5984fa62b53e51906
Kateryna Boikova <boykova.ekaterina.i@gmail.com>
2025-05-15 15:38:04 +0100

    define model

--- packages/server/src/models/index.ts
+++ packages/server/src/models/index.ts
@@ -7,3 +7,5 @@ mongoose
   .connect(`mongodb://${config.host}:${config.port}/${config.nodeEnv}`)
   .then(data => console.log('Connection OK'))
   .catch(err => console.log('err', err));
+
+module.exports.Word = require('./word');
--- packages/server/src/models/word.ts
+++ packages/server/src/models/word.ts
@@ -1,7 +1,17 @@
+import mongoose, { Schema } from 'mongoose';
+
 export interface Word {
   id: number;
   word: [];
-  tag: string;
+  tag?: string;
 }
+
+const wordSchema = new Schema({
+  id: { type: Number },
+  word: { type: Array },
+  tag: { type: String },
+});
+
+const Word = mongoose.model('words', wordSchema);
 
 export let words: Word[] = [];
