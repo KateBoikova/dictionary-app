05a6c53ebc253d0e3732a0878f80280aa2be735c
Kateryna Boikova <boykova.ekaterina.i@gmail.com>
2025-07-14 12:10:09 +0100

    change server connection

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,6 +1,9 @@
 import app from './app';
 import config from './config/config';
+import http from 'node:http';
 
-app.listen(config.port, () => {
+const server = http.createServer(app);
+
+server.listen(config.port, () => {
   console.log(`Server is running on port ${config.port}`);
 });
