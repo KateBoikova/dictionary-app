d5b543b2f73e412fd729cbf60f21d91cdbabeaa2
Kateryna Boikova <boykova.ekaterina.i@gmail.com>
2025-07-14 12:09:07 +0100

    add db configs

--- packages/server/src/config/config.ts
+++ packages/server/src/config/config.ts
@@ -6,12 +6,14 @@ interface Config {
   host: string;
   port: number;
   nodeEnv: string;
+  dbName: string;
 }
 
 const config: Config = {
-  host: '127.0.0.1',
-  port: Number(process.env.PORT) || 3000,
+  host: 'mongodb+srv://boykovaekaterinai:justalearner9@cluster0.w8pkq.mongodb.net/?retryWrites=true&w=majority',
+  port: Number(process.env.PORT) || 5000,
   nodeEnv: process.env.NODE_ENV || 'development',
+  dbName: 'DICTIONARY_APP',
 };
 
 export default config;
